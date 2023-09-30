a4e1e252f43369d48a9f9d056166c36336ce359b
Shahin Shaikh <shahinwithani07@gmail.com>
2023-09-30 18:40:23 +0530

    a

--- src/Components/Sections/Header/Clients.js
+++ src/Components/Sections/Header/Clients.js
@@ -7,7 +7,7 @@ function Clients() {
         <Row style={{paddingTop: "100px", marginTop: "30px"}}>
             <Col className='col-lg-5 col-md-5 col-sm-12 d-flex justify-content-center' style={{paddingLeft: "80px", marginRight: "25px", fontSize: "24px"}}> 
             <Row>
-            <h4 style={{fontSize: "20px", fontWeight: "bold", fontFamily: "raleway"}} className='mx-4 text-left'>UNIQUE & UNISEX</h4>
+            <h4 style={{fontSize: "20px", fontWeight: "bold", fontFamily: "raleway" , letterSpacing: "2px"}} className='mx-4 text-left'>UNIQUE & UNISEX</h4>
             <h5 style={{fontSize: "50px", fontFamily: "league gothic"}} className='mx-4 text-left'>Hipster Series</h5>
            <p style={{ fontFamily: "raleway", fontSize: "20px"}} className='mx-4 text-left'> The Hipster Series compliments all your quirks and crazines. Explore Playful prints, Fun colors and Unique designs that flatters your every move.
             </p> 
@@ -28,7 +28,7 @@ function Clients() {
             </Col>
             <Col className='col-lg-5 col-md-5 col-sm-12 d-flex justify-content-center' style={{paddingRight: "80px", marginRight: "25px", fontSize: "24px"}}> 
             <Row>
-            <h4 style={{fontSize: "20px", fontWeight: "bold", fontFamily: "raleway"}} className='mx-4 text-left'>#BEDRESSPONSIBLE</h4>
+            <h4 style={{fontSize: "20px", fontWeight: "bold", fontFamily: "raleway", letterSpacing: "2px"}} className='mx-4 text-left'>#BEDRESSPONSIBLE</h4>
             <h5 style={{fontSize: "50px", fontFamily: "league gothic"}} className='mx-4 text-left'>The Vagabond</h5>
            <p style={{ fontFamily: "raleway", fontSize: "20px"}} className='mx-4 text-left'> 
            Inspired from the earthy roots of Bohemian Culture, The vagabond collection transcends the realm of ordinary fashion with bewitching bohemian harem pants.
