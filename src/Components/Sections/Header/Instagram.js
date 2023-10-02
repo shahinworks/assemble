f21cc4ac5e7acbdce4eb6bf9c9de7eca6427df8d
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-02 10:04:58 +0530

    cc

--- src/Components/Sections/Header/Instagram.js
+++ src/Components/Sections/Header/Instagram.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function Instagram() {
   return (<>
@@ -6,4 +6,4 @@ function Instagram() {
   </>)
 }
 
-export default Instagram
+export default Instagram;
