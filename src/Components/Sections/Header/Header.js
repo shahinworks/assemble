ac92a18de35a02ad4d0c8b9b97fe6b2ac66a46d7
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-05 13:21:55 +0530

    updates

--- src/Components/Sections/Header/Header.js
+++ src/Components/Sections/Header/Header.js
@@ -2,15 +2,20 @@ import React, { useState , useEffect} from 'react';
 import {Speedometer, List, Person, Search, Cart, Bag} from 'react-bootstrap-icons';
 import { Button, Modal, Card } from 'react-bootstrap';
 import './Header.css';
+import logo1 from './logo-TRP.jpg';
+import logo2 from './logo-TRP.png';
+
 
 function Header() {
   const [header, setHeader] = useState("header");
+  const [logo, setLogo] = useState(logo2);
 
   const listenScrollEvent = event => {
     if (window.scrollY < 73) {
-      return setHeader("header");
+      return setHeader("header"), setLogo(logo2);
+
     } else if (window.scrollY > 70) {
-      return setHeader("header2");
+      return setHeader("header2"), setLogo(logo1);
     }
   };
 
@@ -28,29 +33,29 @@ function Header() {
     <div className="d-flex justify-content-between align-items-center">
     <div id="logo" className='logo'>
     <a href="index.html">
-      <h2 className="text-dark" style={{ textDecoration: "none" }}> LOGO </h2>
+      {/* <h2 className="text-dark" style={{ textDecoration: "none" }}> LOGO </h2> */}
+      <img style={{ height: "100px", width: "100px", borderRadius: "25px"}} src={logo} alt='Logo'/>
     </a>
     {/* Uncomment below if you prefer to use a text logo */}
     {/*<h1><a href="index.html">Regna</a></h1>*/}
   </div>
   {/* <div className="logo">Logo</div> */}
-  <nav id="navbar" className="navbar">
-    <ul className="links">
-      <li><a className="link-item nav-link scrollto" href="/"> Home </a> </li>
-      <li> <a className="link-item nav-link scrollto" href="/shop">  Shop </a> </li>
-      <li> <a className="link-item nav-link scrollto" href="services.html"> Returns/Exchange </a> </li>
-      <li> <a className="link-item nav-link scrollto" href="book.html"> Track </a> </li>
-      <li> <a className="link-item nav-link scrollto" href="photo.html"> FAQ's </a> </li>
-      <li> <a className="link-item nav-link scrollto" href="/about">  About US </a> </li>
+  <nav id="navbar" className="navbar mx-2" >
+    <ul className="links" >
+      <li> <a style={{fontSize: "20px"}} className="link-item nav-link scrollto" href="/"  > Home </a> </li>
+      <li> <a style={{fontSize: "20px"}} className="link-item nav-link scrollto" href="/shop" >  Shop </a> </li>
+      <li> <a style={{fontSize: "20px"}} className="link-item nav-link scrollto" href="services.html" > Returns/Exchange </a> </li>
+      <li> <a style={{fontSize: "20px"}} className="link-item nav-link scrollto" href="book.html" > Track </a> </li>
+      <li> <a style={{fontSize: "20px"}} className="link-item nav-link scrollto" href="photo.html"> FAQ's </a> </li>
+      <li> <a style={{fontSize: "20px"}} className="link-item nav-link scrollto" href="/about">  About US </a> </li>
   </ul>
-  <div className="d-flex">
-    
+  <div className="d-flex mx-2">
+   
   <Person className='mx-1' color='black' size={28} />
  
   <Search  className='mx-1' color='black' size={28} />
   <Bag  onClick={() => setEditModal(true)}  className='mx-1' color='black' size={28} />
-
-    </div>
+  </div> 
   </nav>
   </div>
 </header> 
