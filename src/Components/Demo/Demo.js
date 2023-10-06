ca5f8423798caeba472ccf7947f2d2dab9dbd6f2
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-06 14:51:44 +0530

    cc

--- src/Components/Demo/Demo.js
+++ src/Components/Demo/Demo.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import MediaQuery from 'react-responsive'
 
 function Demo() {
+  // const isDesktopOrLaptop = useMediaQuery({
+  //   query: '(min-width: 1224px)'
+  // });
+  // const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' });
+  // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+  // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+  // const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
+
+
   return (
-    <div>Demo</div>
+    <div>
+    <h1>Device Test!</h1>
+    <MediaQuery minWidth={1224}>
+      <p>You are a desktop or laptop</p>
+      <MediaQuery minWidth={1824}>
+        <p>You also have a huge screen</p>
+      </MediaQuery>
+    </MediaQuery>
+    <MediaQuery minResolution="2dppx">
+      {/* You can also use a function (render prop) as a child */}
+      {(matches) =>
+        matches
+          ? <p>You are retina</p>
+          : <p>You are not retina</p>
+      }
+    </MediaQuery>
+  </div>
+
+  //   <div>
+  //   <h1>Device Test!</h1>
+  //   {isDesktopOrLaptop && <p>You are a desktop or laptop</p>}
+  //   {isBigScreen && <p>You  have a huge screen</p>}
+  //   {isTabletOrMobile && <p>You are a tablet or mobile phone</p>}
+  //   <p>Your are in {isPortrait ? 'portrait' : 'landscape'} orientation</p>
+  //   {isRetina && <p>You are retina</p>}
+  // </div>
   )
 }
 
--- src/Components/Sections/Header/Header.js
+++ src/Components/Sections/Header/Header.js
@@ -4,9 +4,18 @@ import { Button, Modal, Card } from 'react-bootstrap';
 import './Header.css';
 import logo1 from './logo-TRP.jpg';
 import logo2 from './logo-TRP.png';
-
+import { useMediaQuery } from 'react-responsive';
+import MediaQuery from 'react-responsive';
 
 function Header() {
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
   const ice = {
     maxWidth: "1000px",
@@ -43,15 +52,13 @@ function Header() {
     <>
     <header className={header}>
     <div className="d-flex justify-content-between align-items-center">
-    <div id="logo" className='logo'>
+   
+  <MediaQuery minWidth={1200}>
+  <div id="logo" className='logo'>
     <a href="index.html">
-      {/* <h2 className="text-dark" style={{ textDecoration: "none" }}> LOGO </h2> */}
       <img className='mt-2 logo-img' style={{ height: "100px", width: "100px", borderRadius: "25px"}} src={logo} alt='Logo'/>
     </a>
-    {/* Uncomment below if you prefer to use a text logo */}
-    {/*<h1><a href="index.html">Regna</a></h1>*/}
   </div>
-  {/* <div className="logo">Logo</div> */}
   <nav id="navbar" className="navbar mx-2" >
     <ul className="links" >
       <li> <a style={{fontSize: "20px"}} className="link-item nav-link scrollto" href="/"  > Home </a> </li>
@@ -68,22 +75,92 @@ function Header() {
       <li> <Bag  onClick={() => setEditModal(true)}  className='mx-1' color='black' size={28} /></li>
     </ul>   
   </div> 
+  </nav> 
+  </MediaQuery>
+
+
+    <MediaQuery minWidth={1021} maxWidth={1199}>
+    <div id="logo" className='logo'>
+    <a href="index.html">
+      <img className='mt-2 logo-img' style={{ height: "100px", width: "100px", borderRadius: "25px"}} src={logo} alt='Logo'/>
+    </a>
+  </div>
+   
+  {/* <div className="logo">Logo</div> */}
+  <nav id="navbar" className="navbar mx-2" >
+    <ul className="links" >
+      <li> <a style={{fontSize: "12px"}} className="link-item nav-link scrollto" href="/"  > Home </a> </li>
+      <li> <a style={{fontSize: "12px"}} className="link-item nav-link scrollto" href="/shop" >  Shop </a> </li>
+      <li> <a style={{fontSize: "12px"}} className="link-item nav-link scrollto" href="services.html" > Returns/Exchange </a> </li>
+      <li> <a style={{fontSize: "12px"}} className="link-item nav-link scrollto" href="book.html" > Track </a> </li>
+      <li> <a style={{fontSize: "12px"}} className="link-item nav-link scrollto" href="photo.html"> FAQ's </a> </li>
+      <li> <a style={{fontSize: "12px"}} className="link-item nav-link scrollto" href="/about">  About US </a> </li>
+    </ul>
+  <div className="nav-item d-flex mx-2">
+    <ul>
+      <li> <Person className=' mx-1' color='black' size={20} /></li>
+      <li> <Search  className='  mx-1' color='black' size={20} /> </li>
+      <li> <Bag  onClick={() => setEditModal(true)}  className='mx-1' color='black' size={20} /></li>
+    </ul>   
+  </div> 
   </nav>
+  </MediaQuery>
+
+  <MediaQuery minWidth={769} maxWidth={1020}>
+  <div id="logo" className='logo'>
+    <a href="index.html">
+      <img className='mt-2 logo-img' style={{ height: "60px", width: "60px", borderRadius: "15px"}} src={logo} alt='Logo'/>
+    </a>
+  </div>
+   
+   {/* <div className="logo">Logo</div> */}
+   <nav id="navbar" className="navbar mx-2" >
+     <ul className="links" >
+       <li> <a style={{fontSize: "8px"}} className="link-item nav-link scrollto" href="/"  > Home </a> </li>
+       <li> <a style={{fontSize: "8px"}} className="link-item nav-link scrollto" href="/shop" >  Shop </a> </li>
+       <li> <a style={{fontSize: "8px"}} className="link-item nav-link scrollto" href="services.html" > Returns/Exchange </a> </li>
+       <li> <a style={{fontSize: "8px"}} className="link-item nav-link scrollto" href="book.html" > Track </a> </li>
+       <li> <a style={{fontSize: "8px"}} className="link-item nav-link scrollto" href="photo.html"> FAQ's </a> </li>
+       <li> <a style={{fontSize: "8px"}} className="link-item nav-link scrollto" href="/about">  About US </a> </li>
+     </ul>
+   <div className="nav-item d-flex ">
+     <ul>
+       <li> <Person className=' mx-1' color='black' size={20} /></li>
+       <li> <Search  className='  mx-1' color='black' size={20} /> </li>
+       <li> <Bag  onClick={() => setEditModal(true)}  className='mx-1' color='black' size={20} /></li>
+     </ul>   
+   </div> 
+   </nav>
+   </MediaQuery>
+
+
+
+   </div>
+  <MediaQuery maxWidth={768}>
+  <div className="d-flex justify-content-left align-items-start" style={{textAlign: "start", marginLeft: "0px", paddingLeft: "0px"}}>
+  <div id="logo" className='logo'>
+    <a href="index.html">
+      <img className='mt-2 logo-img text-left' style={{ height: "60px", width: "60px", borderRadius: "15px"}} src={logo} alt='Logo'/>
+    </a>
+  </div>
   </div>
 
   {/* <Button style={{marginRight: "1px", paddingRight: "0px", textAlign: "right"}} type="button" variant="outline-link"> */}
-    {!nav &&  <List onClick={() => setNav(true)} className='me-4 mt-0 pt-0 d-lg-none' color='black' size={28}  />}
+    {!nav &&  <List onClick={() => setNav(true)} className='me-4 mt-0 pt-0' color='black' size={28}  />}
     {/* </Button> */}
+    </MediaQuery>
+   
 </header> 
 
 
 
 {/* modal-right scroll-out-negative   */}
 
+<MediaQuery maxWidth={768}>
 <Modal className="px-0 mx-0 toggle" show={nav} onHide={() => setNav(false)} scrollable dialogClassName="full">
-{/* <Modal.Header closeButton className='modalHead'>
-  <Modal.Title className='fw-bold' as="h5">MENU ITEMS</Modal.Title>
-</Modal.Header> */}
+<Modal.Header closeButton className='modalHead'>
+  {/* <Modal.Title className='fw-bold' as="h5">MENU ITEMS</Modal.Title> */}
+</Modal.Header>
 <Modal.Body  style={ice} className='px-0 mx-0 modalBod'>
 <ul className="links nav-item"  style={{listStyle: "none", textAlign: "left"}}>
       <li> <a style={{fontSize: "20px", fontWeight: "bold"}} className="link-item nav-link" href="/"  > Home </a> </li> <hr/>
@@ -97,6 +174,7 @@ function Header() {
 <Modal.Footer className="border-0">
 </Modal.Footer>
 </Modal>
+</MediaQuery>
 
 <Modal className="modal-right scroll-out-negative" show={editModal} onHide={() => setEditModal(false)} scrollable dialogClassName="full">
 <Modal.Header closeButton>
