393fc93ebf04b8b698a567ca5ff65a8c8c4ff2e7
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-09 16:25:54 +0530

    c

--- src/Components/Sections/Footer/Footer.js
+++ src/Components/Sections/Footer/Footer.js
@@ -166,41 +166,40 @@ function Footer() {
   </MediaQuery>
 
 <MediaQuery maxWidth={768} >
-  <div className="bg-dark accordion" id="accordionExample">
-  
-  <div className="bg-dark  accordion-item">
-    <h2 className="accordion-header">
-      <button className="bg-dark  accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+  <div className="accordion" id="accordionExample" style={{backgroundColor: "black", color: "white"}}>
+  <div className="accordion-item"  style={{backgroundColor: "black", color: "white", border: "2px solid black"}}>
+    <h2 className="accordion-header"  style={{backgroundColor: "black", color: "white"}}>
+      <button style={{backgroundColor: "black", color: "white"}} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
         Main Menu
       </button>
     </h2>
-    <div id="collapseTwo" className="bg-dark accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div className="bg-dark accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+    <div style={{backgroundColor: "black", color: "white"}} id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div style={{backgroundColor: "black", color: "white"}} className="accordion-body">
+        <strong style={{backgroundColor: "black", color: "white"}}>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
   </div>
-  <div className="accordion-item ">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+  <div style={{backgroundColor: "black", color: "white", border: "2px solid black"}} className="accordion-item ">
+    <h2 style={{backgroundColor: "black", color: "white"}} className="accordion-header">
+      <button style={{backgroundColor: "black", color: "white"}} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
        Footer Menu
       </button>
     </h2>
-    <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+    <div style={{backgroundColor: "black", color: "white"}} id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div style={{backgroundColor: "black", color: "white"}} className="accordion-body">
+        <strong style={{backgroundColor: "black", color: "white"}}>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
   </div>
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+  <div style={{backgroundColor: "black", color: "white", border: "2px solid black"}} className="accordion-item">
+    <h2 style={{backgroundColor: "black", color: "white"}} className="accordion-header">
+      <button style={{backgroundColor: "black", color: "white"}} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
         Sign up and Save
       </button>
     </h2>
-    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+    <div style={{backgroundColor: "black", color: "white"}} id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div style={{backgroundColor: "black", color: "white"}} className="accordion-body">
+        <strong style={{backgroundColor: "black", color: "white"}}>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
   </div>
