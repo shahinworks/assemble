0768fc81557346f53fb39d3c777f8b716d527b1c
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-10 17:54:04 +0530

    cc

--- src/Components/Admin/Category/ListCategory.js
+++ src/Components/Admin/Category/ListCategory.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
-import { Card, Col, Row, Button } from "react-bootstrap";
-import { gql, useQuery } from "@apollo/client";
+import { Card, Col, Row, Button , Modal, Form} from "react-bootstrap";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import {Pencil, Trash} from 'react-bootstrap-icons';
+import { useState } from "react";
 
 function ListCategory() {
+
+  const [modal, showModal] = useState(false);
 
   const GET_ALL_CATEGORY = gql`
   query GetAllCategory {
@@ -15,15 +18,39 @@ function ListCategory() {
 
   const { data, refetch } = useQuery(GET_ALL_CATEGORY);
 
-  // console.log(data);
-
   useEffect(() => {
     refetch();
-
   }, []);
 
+  // EDIT
 
+  const [categoryName, setCategoryName] = useState('');
+  const [categroryId, setCategoryId] = useState('');
 
+  const EDIT_CATEGORY = gql`
+    mutation UpdateCategory($updateCategoryId: ID, $categoryName: String) {
+      updateCategory(id: $updateCategoryId, categoryName: $categoryName) {
+        id
+        categoryName
+      }
+    }
+  `;
+
+  const [editCategory , {data: dataEdit}] = useMutation(EDIT_CATEGORY);
+
+  function handleEdit(id) {
+    showModal(true);
+    setCategoryId(id);
+  }
+
+  const handleSubmit = async () => {
+    await editCategory({
+      variables: {
+        updateCategoryId: categroryId,
+        categoryName: categoryName
+      }
+    });
+  }
 
   return (
     <>
@@ -48,11 +75,10 @@ function ListCategory() {
                      <td  style={{ backgroundColor: "black", color: "white", border: "1px solid black"}} >{index + 1}</td>
                      <td  style={{ backgroundColor: "black", color: "white", border: "1px solid black"}} >{item.categoryName}</td>
                      <td  style={{ backgroundColor: "black", color: "white", border: "1px solid black"}} >
-                       <Button className="btn btn-sm btn-light mx-2">
+                       <Button className="btn btn-sm btn-light mx-2" onClick={() => handleEdit(item.id)}>
                         <Pencil size={20} color="black"/>
                        </Button>
-                     {/* </td>
-                     <td className="border"> */}
+
                        <Button className="btn btn-sm btn-light"> 
                         <Trash size={20} color="black" />
                        </Button>
@@ -67,6 +93,63 @@ function ListCategory() {
           </Card>
         </Col>
       </Row>
+
+
+      <Modal
+        className="modal-right scroll-out-negative"
+        show={modal}
+        onHide={() => showModal(false)}
+        scrollable
+        dialogClassName="full"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title as="h5">Update Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <h5>Category Name</h5>
+        <Form.Control type="text" name="categoryName" value={categoryName} onChange={(e) => setCategoryName(e.target.value)}/>
+        <Button type='submit' className='my-2 btn btn-light' onClick={() => handleSubmit()}>Submit</Button>
+
+          {/* <OverlayScrollbarsComponent options={{ overflowBehavior: { x: 'hidden', y: 'scroll' } }} className="scroll-track-visible">
+            <Form onSubmit={updateCategory}>
+              {originalCategory && (
+                <div className="mb-3">
+                  <Form.Label>Category Name</Form.Label>
+                  <Form.Control type="text" defaultValue={originalCategory} readOnly />
+                </div>
+              )}
+              <div className="mb-3">
+                <Form.Label>New Category</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={newcatName}
+                  onChange={(e) => {
+                    if (e.target.value.includes('_')) {
+                      e.target.value = e.target.value.replace('_', '');
+                    }
+                    if (e.target.value.includes('/')) {
+                      e.target.value = e.target.value.replace('/', '');
+                    }
+                    setNewCatName(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-3">
+                <Form.Label>New Description</Form.Label>
+                <Form.Control type="text" maxLength="130" value={editDesc} onChange={(e) => setEditDesc(e.target.value)} />
+              </div>
+              <div className="mb-3">
+                <Form.Label>New Image</Form.Label>
+                <Form.Control type="file" accept="image/*" onChange={(e) => setNewCatImage(e.target.files[0])} />
+              </div>
+              <Button variant="primary" type="submit" className="btn-icon btn-icon-start">
+                <CsLineIcons icon="save" />
+                <span>Update</span>
+              </Button>
+            </Form>
+          </OverlayScrollbarsComponent> */}
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
