4b1a214ca7a739fb7fa9053f6db3b95405bcfcff
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-10 12:04:04 +0530

    cc

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,56 @@
-import React from 'react'
+// Layouts
+import AnonymousLayout from "../Layouts/AnonymousLayout";
+import MainLayout from "../Layouts/MainLayout";
 
-function Routes() {
-  return (
-    <div>Routes</div>
-  )
-}
+// Pages
+import Login from "../Pages/Login";
+import Home from "../Pages/Home";
+import CreateUser from "../Pages/CreateUser";
+import ListUsers from "../Pages/ListUser";
 
-export default Routes;
+export const Routes = [
+{
+    layout: AnonymousLayout,
+    routes: [
+      {
+        name: 'login',
+        title: 'Login page',
+        component: Login,
+        path: '/login',
+        isPublic: true,
+      }
+    ]
+  },
+{
+    layout: MainLayout,
+    routes: [
+      {
+        name: 'home',
+        title: 'Home page',
+        component: Home,
+        path: '/home'
+      },
+      {
+        name: 'users',
+        title: 'Users',
+        hasSiderLink: true,
+        routes: [
+          {
+            name: 'list-users',
+            title: 'List of users',
+            hasSiderLink: true,
+            component: ListUsers,
+            path: '/users'
+          },
+          {
+            name: 'create-user',
+            title: 'Add user',
+            hasSiderLink: true,
+            component: CreateUser,
+            path: '/users/new'
+          }
+        ]
+      }
+    ]
+  }
+];
