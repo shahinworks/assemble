d80589796dbb1b6c58f15a1d0bcb8faeb5718c83
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-11 18:12:56 +0530

    c

--- src/Components/Admin/Size/CreateSize.js
+++ src/Components/Admin/Size/CreateSize.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import { useMutation, gql } from "@apollo/client";
+import React, { useState } from "react";
+import { Form, Card, Col, Row, Button } from "react-bootstrap";
+
+
 
 function CreateSize() {
+
+  const [size, setSize] = useState("");
+
+
+
+
+  const CREATE_SIZE_MUTATION = gql`mutation CreateSize($sizeName: String) {
+  createSize(sizeName: $sizeName) {
+    sizeName
+    id
+  }
+}`;
+
+  const [createSize] = useMutation(CREATE_SIZE_MUTATION);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await createSize({
+        variables: {
+          sizeName: size
+        },
+      });
+
+      console.log(data.createSize);
+
+    }
+    catch (err) {
+      console.error(err);
+    }
+  };
+
+
+
   return (
-    <div>CreateSize</div>
-  )
+    <>
+      <Row>
+        <Col className="col-8 mx-auto my-5">
+          <Card className="mb-5">
+            <Card.Body>
+              <h1>create size</h1>
+              <Form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <Form.Label >
+                    Size :
+                  </Form.Label>
+
+                  <Form.Control
+                    type="text"
+
+                    name="categoryName"
+                    value={size}
+                    onChange={(e) => setSize(e.target.value)}
+                  />
+                </div>
+
+                <Button variant="success" className="my-3" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default CreateSize
+export default CreateSize;
