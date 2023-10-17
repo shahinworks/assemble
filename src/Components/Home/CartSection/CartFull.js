32575c86c6f75aeea942acb94e2886ac52f5b469
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-17 12:48:38 +0530

    cc

--- src/Components/Home/CartSection/CartFull.js
+++ src/Components/Home/CartSection/CartFull.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Cart } from 'react-bootstrap-icons';
+import { Button } from 'react-bootstrap';
 
 function CartFull() {
     const { state } = useLocation();
+    const navigate = useNavigate();
     console.log("state", state);
-  return (
-    <div>CartFull</div>
+    const goToCart = () => {
+        navigate('/cart', {state});
+      }
+
+  return (<>
+    <h1>Cart FULL SCREEN PAGE</h1>
+{/* <a href='/cart' > <Cart/>
+</a> */}
+  <Button variant='link' onClick={() => goToCart()}>
+    <Cart/>
+  </Button>
+  </>
+    
   )
 }
 
