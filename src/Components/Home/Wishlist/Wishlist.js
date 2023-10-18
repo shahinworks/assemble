4d762c65f7adef752b3ff0054c6de644e8cb2a55
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-18 14:36:51 +0530

    cc

--- src/Components/Home/Wishlist/Wishlist.js
+++ src/Components/Home/Wishlist/Wishlist.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import { Row, Col, Button } from 'react-bootstrap';
-import { Pencil, Trash } from 'react-bootstrap-icons';
+import { Cart, Heart } from 'react-bootstrap-icons';
+import toast from 'react-hot-toast';
 
 function Wishlist() {  
 
@@ -27,34 +28,99 @@ function Wishlist() {
           }
         }
       }
-    `;
+  `;
 
   const {data} = useQuery(GET_WISHLIST_ITEMS);
   if(data){
     console.log("data", data);
   }
+
+
+  // ADD TO CART FROM WISHLIST
+  
+  const ADD_TO_CART = gql`
+    mutation AddToCart($productId: ID!, $quantity: Int!) {
+      addToCart(productId: $productId, quantity: $quantity) {
+        _id
+      }
+    }
+  `;
+  
+  const [addToCart, {data: cartData}] = useMutation(ADD_TO_CART, {
+    onCompleted : () => {
+      toast.success("Product Added Successfully in cart");
+    },
+    onError : (error) => {
+      toast.error("Error Occured");
+      console.error("ERROR: ", error.message)
+    }
+  });
+  
+  const handleAddToCart = async (id) => {
+    console.log("Add to Cart");
+    await addToCart({
+      variables: {  
+        productId: id,
+        quantity: 1
+      }
+    });
+  }
+
+  // REMOVE FROM WISHLIST
+  const REMOVE_FROM_WISHLIST = gql`
+    mutation RemoveFromWishlist($productId: ID) {
+      removeFromWishlist(productId: $productId) {
+        id
+        userId
+        wishlistProducts {
+          productId {
+            id
+          }
+        }
+      }
+    }
+  `;
+
+  const [removeWishlist, {data: removeData}] = useMutation(REMOVE_FROM_WISHLIST, {
+    onCompleted : () => {
+      toast.success("Removed from Wishlist");
+    }, 
+    onError : (error) => {
+      toast.error("Some Error Occured");
+      console.log("ERROR ", error.message);
+    }
+  });
+
+  const handleRemoveFromWishlist = (id) => {
+    removeWishlist({
+      variables : {
+        productId: id
+      }
+    });
+  }
+
+
   return (<>
-    <div style={{marginTop: "10%"}}>Wishlist Item</div>
+    <h4 style={{marginTop: "10%"}} className='text-center mb-5'>Wishlist Item</h4>
 
       {data && data?.wishlist?.wishlistProducts?.map((wish) => 
       <div key={wish?.productId?.id} className='mb-1 mx-1 px-0 mt-1'>
         <Row className='my-2'>
+          <Col className='col-2' />
           <Col className='col-3'><img style={{height: "100px", width:"70px", border: "2px solid black"}} src={wish?.productId?.images } alt="s"/></Col>
-          <Col className='col-9'> <p className='fs-6'>{wish?.productId?.productName}</p>
+          <Col className='col-4'> <p className='fs-6'>{wish?.productId?.productName}</p>
             <Row>
               <Col className='fw-bold'>₹ {wish?.productId?.sellingPrice}</Col>
             </Row>
           </Col>
-          <Col>
+          <Col className='col-3'>
           <Button className="btn btn-sm btn-light" 
-        //  onClick={() => handleEdit(item.id, item.content, item.url )}
-          >
-            <Pencil size={20} color="black"/>
-          </Button>{" "}
-          <Button className="btn btn-sm btn-light" 
-          // onClick={() => handleDelete(item.id)}
-          >
-            <Trash size={20} color="black" />
+            onClick = {() => handleAddToCart(wish?.productId?.id)} >
+            <Cart size={20} color="black"/>
+          </Button>
+          <Button className="btn btn-sm btn-light"
+            onClick={() => handleRemoveFromWishlist(wish?.productId?.id)} >
+            <Heart size={20} color="black" />
           </Button>
           </Col>
         </Row>
