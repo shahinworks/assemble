e21bbab879d346a3a18f624ece7ac388b770b521
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-19 19:26:20 +0530

    cc

--- src/Components/SuperComponent/ResetPassword.js
+++ src/Components/SuperComponent/ResetPassword.js
@@ -1,9 +1,46 @@
+import { gql, useMutation } from '@apollo/client';
 import React from 'react'
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { Button } from 'react-bootstrap';
 
 function ResetPassword() {
-  return (
-    <div>ResetPassword</div>
-  )
+const [ passWord, setPassWord] = useState("");
+
+
+    const RESET_PASSWORD = gql`
+    mutation ResetPassword($token: String!, $newPassword: String!) {
+        resetPassword(token: $token, newPassword: $newPassword)
+      }
+      `;
+
+
+      const [ reset, {data}] = useMutation(RESET_PASSWORD, {
+        onCompleted: () => {
+            toast.success("Password Set Successfully")
+        }, 
+        onError: (error) => {
+toast.error("Some Error Occured");
+console.log(error.message);
+        }
+      });
+
+      
+   const handleMail = async () => {
+    await reset({
+        variables: { 
+        token: "null",
+        newPassword: passWord
+      }
+    })
+    
+   }
+
+  return (<>
+    <div>Reset Password</div>
+    <input type='password' onChange={(e) => setPassWord(e.target.value)} />
+    <Button onClick={() =>  handleMail()}>Send</Button>
+</>)
 }
 
 export default ResetPassword;
