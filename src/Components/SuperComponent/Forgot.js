db2785f0d1b6645884801191f79aff719910edbf
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-19 19:16:37 +0530

    cc

--- src/Components/SuperComponent/Forgot.js
+++ src/Components/SuperComponent/Forgot.js
@@ -2,15 +2,22 @@ import { gql, useMutation } from '@apollo/client'
 import React from 'react'
 import { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function Forgot() {
+
+    const navigate = useNavigate();
 const [mail, setMail] = useState("");
     const REQUEST = gql`
     mutation RequestPasswordReset($email: String!) {
         requestPasswordReset(email: $email)
       }`;
 
-      const [sendMail, {data}] = useMutation(REQUEST);
+      const [sendMail, {data}] = useMutation(REQUEST, {
+        onCompleted: () => {
+          navigate('/reset');
+        }
+      });
       const handleMail = async () => {
        await sendMail({
           variables: {
