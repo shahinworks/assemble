09370c34301ca30d206e666e82bd7ce51cba5cde
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-20 11:05:05 +0530

    Changes

--- src/Components/Admin/Order/ListOrder.js
+++ src/Components/Admin/Order/ListOrder.js
@@ -1,9 +1,51 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 
 function ListOrder() {
+  const LIST_ORDER = gql`
+    query GetAllOrder {
+        getAllOrder {
+          id
+          paymentId
+          paymentMethod
+          paymentProof
+          paymentStatus
+          status
+          totalAmount
+          orderProducts {
+            productId {
+              id
+              productName
+              priveiwName
+              sellingPrice
+              images
+              size
+              color
+              gender
+              discount
+              gst
+              description
+              stock
+            }
+            price
+            quantity
+            packedImage
+            shippedImage
+            shippedBy
+            trackingNo
+            trackingUrl
+          }
+        }
+      }
+  `;
+
+  const {data} = useQuery(LIST_ORDER);
+  if(data) {
+    console.log("data", data);
+  }
   return (
     <div>ListOrder</div>
   )
 }
 
-export default ListOrder
+export default ListOrder;
