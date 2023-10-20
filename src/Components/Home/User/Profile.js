60ecf02db467f1852401a98c6b1becc0ab94f5c5
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-20 11:57:00 +0530

    CC

--- src/Components/Home/User/Profile.js
+++ src/Components/Home/User/Profile.js
@@ -1,10 +1,11 @@
 import { gql, useLazyQuery, useMutation, useQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react'
-import { Button, Form, Card, Modal, Row, Col } from 'react-bootstrap';
+import { Button, Form, Card, Modal, Row, Col, Badge } from 'react-bootstrap';
 import * as Yup from 'yup';
 import { useFormik } from 'formik';
 import toast from 'react-hot-toast';
 import { PencilSquare, HouseAdd } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 
 function Profile() {
 
@@ -272,7 +273,7 @@ function Profile() {
       }
     `;
 
-    const [getUserAllOrder, {data: UserOrders}] = useLazyQuery(USER_ORDERS);
+    const [getUserAllOrder, {data: orderData}] = useLazyQuery(USER_ORDERS);
 
     useEffect(() => {
       getUserAllOrder({
@@ -282,8 +283,8 @@ function Profile() {
       })
     }, []);
 
-    if(UserOrders){
-      console.log("UserOrders", UserOrders);
+    if(orderData){
+      console.log("UserOrders", orderData);
     }
 
   return (<>
@@ -315,7 +316,78 @@ function Profile() {
 
         </Col>
         <Col className="col-5 mx-2">
-        Orders of User
+        <Row className="g-0 align-content-center d-none d-lg-flex ps-5 pe-5 mb-2 custom-sort" style={{ backgroundColor: "black", color: "white", border: "1px solid black"}}>
+        <Col  md="2" className="d-flex flex-column mb-lg-0 pe-1 justify-content-center">
+          <div className=" text-md cursor-pointer sort" >
+           Order ID
+          </div>
+        </Col>
+        <Col  md="3" className="d-flex flex-column pe-1 justify-content-center ">
+          <div className=" text-md cursor-pointer sort" >
+            NAME
+          </div>
+        </Col>
+        <Col  md="2" className="d-flex flex-column pe-1 justify-content-center">
+          <div className=" text-md cursor-pointer sort"  >
+           Amount
+          </div>
+        </Col>
+        <Col md="2" className="d-flex flex-column pe-1 justify-content-center">
+          <div className=" text-md cursor-pointer sort"  >
+            Date
+          </div>
+        </Col>
+
+        <Col md="2" className="d-flex flex-column pe-1 justify-content-center">
+          <div className=" text-md cursor-pointer sort"  >
+           Status
+          </div>
+        </Col>
+        {/* <Col lg="1" className="d-flex flex-column pe-1 justify-content-center align-items-lg-center">
+          <div className=" text-md cursor-pointer ">Check</div>
+        </Col> */}
+      </Row>
+
+   {orderData && orderData?.getUserAllOrder?.length > 0 ?
+        orderData?.getUserAllOrder?.map((order, index) => (
+          <Card key={index} className="mb-2 hover-border-primary mx-2" style={{ backgroundColor: "black", color: "white", border: "1px solid black"}}>
+            <Card.Body className="pt-0 pb-0 sh-21 sh-md-8 my-3" style={{ backgroundColor: "black", color: "white", border: "1px solid black"}}>
+              <Row className="g-0 align-content-center cursor-default">
+                <Col xs="11" md="2" className="d-flex flex-column justify-content-center mb-2 mb-md-0 order-1 order-md-1 h-md-100 position-relative">
+                  <div className="text-muted text-small d-md-none">Id</div>
+                  <Link to={`/admin/order/detail/${order?.id}`} className="text-truncate h-100 d-flex align-items-center">
+                    <span maxLength={2}>{order?.id?.substring(0, 12)}...</span>
+                  </Link>
+                </Col>
+                <Col xs="6" md="3" className="d-flex flex-column justify-content-center mb-2 mb-md-0 order-3 order-md-2">
+                  <div className="text-muted text-small d-md-none">Name</div>
+                  <div className="text-alternate">
+                    {order?.user?.firstName} {order?.user?.lastName}
+                  </div>
+                </Col>
+                <Col xs="6" md="2" className="d-flex flex-column justify-content-center mb-2 mb-md-0 order-4 order-md-3">
+                  <div className="text-muted text-small d-md-none">Purchase</div>
+                  <div className="text-alternate">
+                    <span>
+                      <span className="text-small">₹ </span>
+                      {order?.totalAmount}
+                    </span>
+                  </div>
+                </Col>
+                <Col xs="6" md="2" className="d-flex flex-column justify-content-center mb-2 mb-md-0 order-5 order-md-4">
+                  <div className="text-muted text-small d-md-none">Date</div>
+                  <div className="text-alternate">13.09.2023</div>
+                </Col>
+                <Col xs="6" md="2" className="d-flex flex-column justify-content-center mb-2 mb-md-0 order-last order-md-5">
+                  <div className="text-muted text-small d-md-none">Status</div>
+                  <div>
+                    <Badge className="badge bg-dark">{order?.status}</Badge>
+                  </div>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        ) )  : <h2 className='text-center my-4 py-4'>Order Not Found</h2>}
         
         </Col>
 
