ccf8b190070e2b86797f762ef9c67b06d7616dfe
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-20 12:40:20 +0530

    cc

--- src/Components/Sections/Header/Header.js
+++ src/Components/Sections/Header/Header.js
@@ -329,12 +329,25 @@ function Header() {
 
    <Modal className="modal-right scroll-out-negative" show={profileModal} onHide={() => setProfileModal(false)} scrollable dialogClassName="full">
     <Modal.Header closeButton>
-  <Modal.Title className='fw-bold' as="h5">Cart</Modal.Title>
+  <Modal.Title className='fw-bold' as="h5">Profile</Modal.Title>
     </Modal.Header>
     <Modal.Body className='mx-0 px-0'>
+      <ul>
+        <li style={{listStyle: "none", fontSize: "20px"}} className='fw-bold' onClick={() => goToProfile()} >Profile</li>
+        <li style={{listStyle: "none", fontSize: "20px"}} className='fw-bold' >Order</li>
+      </ul>
+
+      <Button style={{backgroundColor: "black", color: "white", border: "1px solid black"}} className='mb-1 fs-5 fw-bold w-100' onClick={() => goToLoginPage()} >LOGIN</Button> 
+      <Button style={{backgroundColor: "black", color: "white", border: "1px solid black"}} className='mt-1 fs-5 fw-bold w-100' onClick={() => goToLoginPage()} >LOGOUT</Button> 
+
+
+    {/* <Button onClick={() => goToLoginPage()}  style={{borderRadius: "50px", backgroundColor: "black", border: "1px solid black"}} > 
+     <Person onClick={() => goToLoginPage()} color='white' size={34} />  
+      </Button>
+      <br />
       THIS is your Profile Modal
       <Button onClick={() => goToLoginPage()}>Login</Button>
-      <Button onClick={() => goToProfile()}>Profile Section</Button>
+      <Button onClick={() => goToProfile()}>Profile Section</Button> */}
     </Modal.Body>
     <Modal.Footer className="border-0"></Modal.Footer>
   </Modal>
