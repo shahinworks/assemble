e079ddd033c8270f1537e56356cd2341a415acd7
Shahin Shaikh <shahinwithani07@gmail.com>
2023-10-23 15:29:10 +0530

    change

--- src/Components/SuperComponent/ResetPassword.js
+++ src/Components/SuperComponent/ResetPassword.js
@@ -2,7 +2,7 @@ import { gql, useMutation } from '@apollo/client';
 import React from 'react'
 import { useState } from 'react';
 import toast from 'react-hot-toast';
-import { Button } from 'react-bootstrap';
+import { Button, Form, Row, Col, Card, Container } from 'react-bootstrap';
 
 function ResetPassword() {
 const [ passWord, setPassWord] = useState("");
@@ -37,9 +37,22 @@ console.log(error.message);
    }
 
   return (<>
-    <div>Reset Password</div>
-    <input type='password' onChange={(e) => setPassWord(e.target.value)} />
-    <Button onClick={() =>  handleMail()}>Send</Button>
+    <div>
+      <Container>
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <div className="border border-3 border-primary"></div>
+            <Card className="shadow" style={{border: "none"}}>
+              <Card.Body className='mx-3'>
+                <h3>Reset Password</h3>
+                <Form.Control type='password' onChange={(e) => setPassWord(e.target.value)} placeholder='Enter New Password' />
+                <Button onClick={() =>  handleMail()} className='mt-4 mb-4'>Submit</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
 </>)
 }
 
