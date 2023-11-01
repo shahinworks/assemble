c00873e94c0b77fc008ac0e1b76edef2706b92eb
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-01 16:56:24 +0530

    c

--- src/Components/Sections/Return.js
+++ src/Components/Sections/Return.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'react-bootstrap';
 
 function Return() {
-  return (
-    <div>Return</div>
-  )
+  return (<>
+    <section className="slider" style={{ paddingTop: "10%" }}>
+    <h1 className='text-center'>Returns & Exchange</h1> 
+    <Card style={{border: "none"}}>
+      <Card.Body className='mx-lg-5'>
+        
+      </Card.Body>
+    </Card>
+    </section>
+        
+  </>);
 }
 
 export default Return
