8d3c1dca0b7edf66b82367ccd972022618e001d1
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-02 16:33:01 +0530

    C

--- src/Components/Home/CartSection/CartPage.js
+++ src/Components/Home/CartSection/CartPage.js
@@ -161,6 +161,27 @@ const handleRemove = async (id, color, gender, size) => {
     getCartData();
   }, [cartValue]);
 
+  const [tempCartValues, setTempCartValues] = useState([]);
+  useEffect(() => {
+    const temp =  cartData?.cart?.cartProducts?.map((item) => {
+      const unit = item?.productId?.images?.filter((r) => r.color === item.color && r.gender === item?.gender);
+        return {
+          image : unit[0]?.imagePath[0],
+          color: item?.color ,
+          size: item?.size ,
+          gender: item?.gender,
+          productName: item?.productId?.productName,
+          quantity : item?.quantity,
+          sellingPrice : item?.productId?.sellingPrice,
+          id: item?.productId?.id
+          
+        }
+      });
+
+      setTempCartValues(temp);
+
+  }, [cartData]);
+
   useEffect(() => {
     setSum(cartData?.cart?.cartProducts?.reduce((acc, curr) => acc + curr?.quantity * curr?.productId?.sellingPrice, 0));
     // const sum = cartData?.cart?.cartProducts?.reduce((acc, curr) => acc + curr?.quantity * curr?.productId?.sellingPrice, 0);
@@ -172,21 +193,23 @@ const handleRemove = async (id, color, gender, size) => {
 //  console.log("sum", sum);
 
   return (<>
-    {cartData?.cart?.cartProducts?.length > 0 && cartData?.cart?.cartProducts?.map((item, index) =>  
+    {cartData?.cart?.cartProducts?.length > 0 && tempCartValues?.map((item, index) =>  
     <div key={index} className='mb-1 mx-1 px-0 mt-1'>
       <Row className='my-2'>
-        <Col className='col-3'><img style={{height: "100px", width:"70px", border: "2px solid black"}} src={item?.productId?.images[0]?.imagePath[0] } alt="s"/></Col>
+        <Col className='col-3'>
+          <img style={{height: "100px", width:"70px", border: "2px solid black"}} 
+          src={item?.image} alt="s"/></Col>
         <Col className='col-9'>
-          <div className='fs-6'>{item?.productId?.priveiwName} 
-          <Button onClick={() => handleRemove(item?.productId?.id, item?.color, item?.gender, item?.size )} style={{marginRight: "0px", border: "none"}} className='my-0 py-0 d-inline me-0 ms-5' variant='outline-danger' > X </Button>
+          <div className='fs-6'>{item?.productName} 
+          <Button onClick={() => handleRemove(item?.id, item?.color, item?.gender, item?.size )} style={{marginRight: "0px", border: "none"}} className='my-0 py-0 d-inline me-0 ms-5' variant='outline-danger' > X </Button>
          </div> 
           <Row>
             <Col>
-              <Button variant='outline-dark' disabled={item?.quantity <= 1} style={{border: "none"}} onClick={() => CartDecrement(item?.productId?.id, item?.size, item?.gender, item?.color)}>-</Button>
+              <Button variant='outline-dark' disabled={item?.quantity <= 1} style={{border: "none"}} onClick={() => CartDecrement(item?.id, item?.size, item?.gender, item?.color)}>-</Button>
                 <input  onChange={(e) => setQuantity(e.target.value)} value={item?.quantity} className="mx-1" style={{background: "none", border: "none", width: "30%", textAlign: "center"}} type='text'  min="0" pattern="[0-9]*"/>
-              <Button variant='outline-dark' style={{border: "none"}} onClick={() => CartIncrement(item?.productId?.id, item?.size, item?.gender, item?.color)}>+</Button>
+              <Button variant='outline-dark' style={{border: "none"}} onClick={() => CartIncrement(item?.id, item?.size, item?.gender, item?.color)}>+</Button>
             </Col>
-            <Col className='fw-bold'>₹ {item?.quantity * item?.productId?.sellingPrice}</Col>
+            <Col className='fw-bold'>₹ {item?.quantity * item?.sellingPrice}</Col>
           </Row>
         </Col>
       </Row>
