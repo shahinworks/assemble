642998e45bdb70b82bab6607b25a022d0764f206
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-02 18:33:48 +0530

    C

--- src/Components/Admin/User/User.js
+++ src/Components/Admin/User/User.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useMutation } from '@apollo/client';
 import { Card, Col, Row, Button , Modal, Form} from "react-bootstrap";
 import {Pencil, Trash} from 'react-bootstrap-icons';
+import toast from 'react-hot-toast';
 
 
 function User() {
@@ -20,7 +21,40 @@ function User() {
     }
   `;
 
-  const {data} = useQuery(GET_ALL_USER);
+  const {data, refetch} = useQuery(GET_ALL_USER);
+
+
+  const DELETE_USER = gql`
+    mutation DeleteUser($deleteUserId: ID!) {
+      deleteUser(id: $deleteUserId) {
+        id
+        firstName
+        email
+      }
+    }
+  `;
+
+  const [deleteUser, {data: deleteUserData}] = useMutation(DELETE_USER, {
+    onCompleted : () => {
+      // toast.success(deleteUserData?.deleteUser?.firstName);
+      toast.success("User Deleted Successfully");
+      refetch();
+    },
+    onError : (error) => {
+      toast.error(error.message);
+    }
+  });
+
+  const handleDeleteUser = async (id, name) => {
+    const shouldDelete = window.confirm(`Are you sure you want to delete ${name}'s Details?`);
+    if(shouldDelete) {
+      await deleteUser({
+        variables: {
+          deleteUserId: id
+        }
+      });
+    }    
+  }
 
   return (<>
     <Row>
@@ -52,7 +86,7 @@ function User() {
                         <Pencil size={20} color="black"/>
                        </Button>
 
-                       <Button className="btn btn-sm btn-light"  > 
+                       <Button className="btn btn-sm btn-light" onClick={() => handleDeleteUser(user.id, user.firstName)}  > 
                         <Trash size={20} color="black" />
                        </Button>
                      </td>
