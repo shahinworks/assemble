6504a7050ccf9117b2059ffc2d7e562896cba619
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-03 16:45:34 +0530

    c

--- src/Components/Home/Order/OrderList.js
+++ src/Components/Home/Order/OrderList.js
@@ -1,6 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { gql, useLazyQuery, useMutation, useQuery } from '@apollo/client';
 
 function OrderList() {
+ 
+  const USER_ORDERS = gql`
+    query GetUserorder {
+        getUserorder {
+          id
+          paymentMethod
+          status
+          totalAmount
+          billingAddress {
+            id
+            firstName
+            lastName
+            mobileNo
+            addressLine1
+            addressLine2
+            city
+            state
+            postalCode
+            country
+          }
+          paymentId
+          paymentStatus
+          shippingAddress {
+            id
+            firstName
+            lastName
+            mobileNo
+            addressLine1
+            addressLine2
+            city
+            state
+            postalCode
+            country
+          }
+          user {
+            id
+            firstName
+            lastName
+            email
+            profilepic
+            mobileNo
+            password
+            role
+          }
+          paymentProof
+          orderProducts {
+            productId {
+              id
+              productName
+              priveiwName
+              sellingPrice
+              size
+              color
+              gender
+              discount
+              gst
+              description
+              stock {
+                quantity
+                gender
+                color
+                size
+              }
+              images {
+                imagePath
+                color
+                gender
+              }
+            }
+            price
+            quantity
+            packedImage
+            shippedImage
+            shippedBy
+            trackingNo
+            trackingUrl
+          }
+        }
+      }
+    `;
+  const {data: orderData} = useQuery(USER_ORDERS);
+
+  if(orderData){
+    console.log("UserOrders", orderData);
+  }
+
   return (
     <div>OrderList</div>
   )
