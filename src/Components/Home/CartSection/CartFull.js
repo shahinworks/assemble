a81772b3756af5c5f427c801177bfd90ce924997
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-06 11:14:56 +0530

    C

--- src/Components/Home/CartSection/CartFull.js
+++ src/Components/Home/CartSection/CartFull.js
@@ -317,8 +317,8 @@ function CartFull() {
    
   
     <Row className='my-2'>
-      <Col className='col-md-8 col-1' />
-      <Col className='col-md-auto col-10 ' >
+      <Col className='col-md-8 col-3' />
+      <Col className='col-md-auto col-8 ' >
       <p className='fw-bold d-inline fs-5' style={{marginRight: "0", paddingRight: "0"}}>SUBTOTAL </p>
       <p className='fw-bold d-inline fs-5' style={{marginRight: "0", paddingRight: "0", alignItems: "end", alignContent: "end"}}> ₹ {sum}</p>
       </Col>
