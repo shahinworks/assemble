fe41d90cc6763849fc2eff5c4e7775af3b55ecbb
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-06 10:53:30 +0530

    C

--- src/Components/Home/Checkout/Checkout.js
+++ src/Components/Home/Checkout/Checkout.js
@@ -630,7 +630,7 @@ const {data: addressByUser, refetch: refetchAdd} = useQuery(SHOW_ALL_ADDRESS_BY_
     {/* <h3 className='mt-5 mb-5'>Checkout</h3> */}
     
      
-   {!showProcessBtn && <Row className='mt-5 mb-5 mx-4 px-4'> 
+    {!showProcessBtn && <Row className='mt-5 mb-5 mx-4 px-4'> 
         <Button  disabled={payNowCheck} className='py-3 ' style={{backgroundColor: "black", color: "white", fontSize: "20px", border: "1px solid black"}} onClick={() => { handleOrder(); setShowProcessBtn(true)}}>PAY NOW</Button>
     </Row>}
 
