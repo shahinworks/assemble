0758458d0888345190b4bcb65ab0d622b0b0a611
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-17 12:25:57 +0530

    c

--- src/Components/Sections/FooterComponent/Refund.js
+++ src/Components/Sections/FooterComponent/Refund.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function Refund() {
-  return (
-    <div>Refund</div>
-  )
+  return (<>
+  <div style={{ paddingTop: "10%" }}>
+  <h1 className='text-center'>  Refund </h1> </div>
+  </>)
 }
 
 export default Refund
