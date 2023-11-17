d4af5af2f4149f2c498423d77313dd9f189032aa
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-17 16:08:01 +0530

    c

--- src/Components/Sections/FooterComponent/Privacy.js
+++ src/Components/Sections/FooterComponent/Privacy.js
@@ -3,6 +3,18 @@ import React from 'react';
 
 function Privacy() {
 
+    // Steps: GO TO nocodeapi and 
+    // create instagram api
+
+    // GO TO meta developer site
+    // create a new project 
+    // go to instagram BASIC API
+    // create a test instagram user
+    // give access
+    // get token
+
+    // paste token in nocodeapi
+
   const demo =  fetch('https://v1.nocodeapi.com/shahinwithani/instagram/wVLopYpTsNwJbNhh').then((response) => response.json()).then((data) => console.log(data));
     const media_url = "image link";
     const permalink = "post link";
