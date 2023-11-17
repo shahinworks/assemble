e67b0f89a8f327995ae311ba82e05ae36c2f5c04
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-17 17:52:38 +0530

    c

--- src/Components/Sections/FooterComponent/Refund.js
+++ src/Components/Sections/FooterComponent/Refund.js
@@ -2,61 +2,93 @@ import React from 'react'
 
 function Refund() {
   return (<>
-   <div className='text-left px-3 m-auto mt-5' style={{ paddingTop: "10%", border: "none", width: "70%", borderRadius: "5px"}}>
-  <h1 className='text-center'>  Refund </h1> 
-   
-  FAQs. General Information
-How long will my order take to ship?
-We usually ship them within 24-48 hours.
-In case any product says Out Of Stock or Pre-Order, it means the product is in manufacturing and will arrive within 2-
-3 weeks. We notify you and manage your waiting list on a first-come-first-served basis.
-On rare occasions, a specific size/color combination may not be available right away. In that case, kindly allow up
-to 10 days to refill our T-shirt inventory before you can place your order.
-My payment was successful but the order status shows Pending Payment/Failed. Why?
-You may have experienced an error/glitch on the payment gateway page or technical difficulties due to which our
-website couldn't capture your transaction (even though it's successful).
-In this case, we request you to contact us via email-operations@theveshticompany.com. We will resolve this by
-manually processing your order.
-Why are a few of the products out of stock?
-Our product designs are in the experimental phase now, so we have them on our website as a showcase of our
-collections. We also have the fast selling products that we keep re-stocking that are available on the website.
-
-However, we do restock the out-of-stock products at different intervals and hence we have them on showcase to help
-our customers understand the collection we have.
-We request you to follow our social media for updates when the out of stock products will be available once again.
-Cancellations and Refunds
-We do our level best to make sure you are satisfied with our product and services. But, if for some unforeseen reason
-you wish to cancel your order, you may do so within 15 days of delivery and get a full refund.
-Kindly raise a return or exchange request through our returns center or email us
-at operations@theveshticompany.com and we will take this forward. Refunds of online payments are processed
-automatically via the payment method used. It can take 3-5 working days for it to reflect in your account once
-processed.
-COD refunds can take up to 5 days from date of delivery of the returned item.
-Orders placed from Countries apart from India can't be cancelled once shipped.
-How long will my order take to deliver, once shipped?
-We partner with the best courier companies globally and will choose the one that is most efficient in your PIN code.
-It takes 2-3 working days for a shipment to reach metro cities in India.
-Up to 5 working days to any non-metro locations in India. International shipments take 10-14 working days to reach
-most western countries.
-We will provide you with tracking information once your order is shipped so you can track the real-time progress of
-your shipment.
-For International orders, We deliver between 3 - 15 days.
-What is your return policy?
-We provide 15-days returns for all products.
-To initiate a return, kindly raise a return or exchange request through our returns center or email us
-at operations@theveshticompany.com describing your reason for return and we will take it forward from there.
-Items will have to be in new condition, without any wash or usage, and to be returned in their original packaging
-within 15 days of delivery.
-
-Orders placed from Countries apart from India can't be returned. However in a rare instance of a manufacturing
-defect in your product, your money will be refunded only to your original payment method.
+    <div className='text-left px-3 m-auto mt-5' style={{ paddingTop: "10%", border: "none", width: "70%", borderRadius: "5px"}}>
+      <h1 className='text-center'> FAQs. General Information </h1>
+        <p className='text-left mx-5'>  How long will my order take to ship?</p>
+        <p className='text-left mx-5'>We usually ship them within 24-48 hours.</p>
+        <p className='text-left mx-5'> In case any product says Out Of Stock or Pre-Order, it means the product is in manufacturing and will arrive within 2-
+          3 weeks. We notify you and manage your waiting list on a first-come-first-served basis.</p>
+        <p className='text-left mx-5'>On rare occasions, a specific size/color combination may not be available right away. In that case, kindly allow up
+          to 10 days to refill our T-shirt inventory before you can place your order.</p>
+        <p className='text-left mx-5'> My payment was successful but the order status shows Pending Payment/Failed. Why?</p>
+        <p className='text-left mx-5'> You may have experienced an error/glitch on the payment gateway page or technical difficulties due to which our
+        website couldn't capture your transaction (even though it's successful).</p>
+        <p className='text-left mx-5'>In this case, we request you to contact us via email-operations@theveshticompany.com. We will resolve this by
+        manually processing your order.</p>
+        <p className='text-left mx-5'> Why are a few of the products out of stock?</p>
+        <p className='text-left mx-5'>Our product designs are in the experimental phase now, so we have them on our website as a showcase of our
+        collections. We also have the fast selling products that we keep re-stocking that are available on the website.</p>
+        <p className='text-left mx-5'>
+        However, we do restock the out-of-stock products at different intervals and hence we have them on showcase to help
+        our customers understand the collection we have.</p>
+        <p className='text-left mx-5'> We request you to follow our social media for updates when the out of stock products will be available once again.
+        Cancellations and Refunds</p>
+        <p className='text-left mx-5' >We do our level best to make sure you are satisfied with our product and services. But, if for some unforeseen reason
+        you wish to cancel your order, you may do so within 15 days of delivery and get a full refund.</p>
+        <p className='text-left mx-5'>Kindly raise a return or exchange request through our returns center or email us
+          at operations@theveshticompany.com and we will take this forward. Refunds of online payments are processed
+          automatically via the payment method used. It can take 3-5 working days for it to reflect in your account once
+          processed.</p>
+          <p className='text-left mx-5'> COD refunds can take up to 5 days from date of delivery of the returned item.</p>
+          <p className='text-left mx-5'>Orders placed from Countries apart from India can't be cancelled once shipped.</p>
+          <p className='text-left mx-5'>How long will my order take to deliver, once shipped?</p>
+          <p className='text-left mx-5'>We partner with the best courier companies globally and will choose the one that is most efficient in your PIN code.</p>
+          <p className='text-left mx-5'>It takes 2-3 working days for a shipment to reach metro cities in India.</p>
+          <p className='text-left mx-5'>Up to 5 working days to any non-metro locations in India. International shipments take 10-14 working days to reach
+most western countries.</p>
+          <p className='text-left mx-5'> We will provide you with tracking information once your order is shipped so you can track the real-time progress of
+your shipment.</p>
+          <p className='text-left mx-5'> For International orders, We deliver between 3 - 15 days.</p>
+          <p className='text-left mx-5'> What is your return policy?</p>
+          <p className='text-left mx-5'> We provide 15-days returns for all products.</p>
+          <p className='text-left mx-5'> To initiate a return, kindly raise a return or exchange request through our returns center or email us
+at operations@theveshticompany.com describing your reason for return and we will take it forward from there.</p>
+          <p className='text-left mx-5'>Items will have to be in new condition, without any wash or usage, and to be returned in their original packaging
+within 15 days of delivery.</p>
+          <p className='text-left mx-5'> Orders placed from Countries apart from India can't be returned. However in a rare instance of a manufacturing
+defect in your product, your money will be refunded only to your original payment method.</p>
+          <p className='text-left mx-5'> 
 My Product is missing from the package and the order status shows completed. Why?
 In a rare occasion that you havn't received your entire order, we request you to please mail us at
-operations@theveshticompany.com and we'll resolve your query before 48 hours.
-Can you ship faster if I'm traveling somewhere or have to gift to someone?
-Yes, since most of our products are made in-house, we do take special requests to expedite certain orders. We do not
+operations@theveshticompany.com and we'll resolve your query before 48 hours.</p>
+<p className='text-left mx-5'>Can you ship faster if I'm traveling somewhere or have to gift to someone? </p>
+
+<p className='text-left mx-5'>Yes, since most of our products are made in-house, we do take special requests to expedite certain orders. We do not
 provide any guarantee but rest assured, we will always try our best to accommodate any urgent orders.
-Just reach us out on the contact info provided and we'll let you know 
+Just reach us out on the contact info provided and we'll let you know  </p>
+<p className='text-left mx-5'>  </p>
+<p className='text-left mx-5'> </p>
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   </div>
   </>)
   
