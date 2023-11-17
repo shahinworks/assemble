b350521cd70e10ce485b9f60adb049f34ed68b60
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-17 17:05:55 +0530

    c

--- src/Components/Sections/FooterComponent/Privacy.js
+++ src/Components/Sections/FooterComponent/Privacy.js
@@ -1,90 +1,104 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 function Privacy() {
 
   return (<>
+    <div className='text-left px-3 m-auto mt-5' style={{ paddingTop: "10%", border: "none", width: "70%", borderRadius: "5px"}}>
+      <Card>
+        <Card.Body> 
+        <h1 className='text-center fw-bold' > Privacy Policy</h1>
+        </Card.Body>
+      </Card>
+    </div>
     <div style={{ paddingTop: "10%" }}>
-      <h1 className='text-center'> Privacy </h1>
-      Privacy Policy
-The Veshti Company Privacy Policy
-This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a
-purchase from https://theveshticompany.com (the "Site").
-PERSONAL INFORMATION WE COLLECT
-When you visit the Site, we automatically collect certain information about your device, including information about
-your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as
-you browse the Site, we collect information about the individual web pages or products that you view, what websites
-or search terms referred you to the Site, and information about how you interact with the Site. We refer to this
-automatically-collected information as "Device Information."
-We collect Device Information using the following technologies:
+      <h1 className='text-center fw-bold'> Privacy Policy </h1>
+      <p className='text-left mx-5'> The Veshti Company Privacy Policy </p>
+        <br/>
+        <p className='text-left mx-5'>This Privacy Policy describes 
+      how your personal information is collected, used, and shared when you visit or make a
+        purchase from https://theveshticompany.com (the "Site"). </p>
+     <br/> 
+     <p className='text-left mx-5'>PERSONAL INFORMATION WE COLLECT </p>
+      <br/>
+   <p className='text-left mx-5'> When you visit the Site, we automatically collect certain information about your device, including information about
+      your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as
+      you browse the Site, we collect information about the individual web pages or products that you view, what websites
+      or search terms referred you to the Site, and information about how you interact with the Site. We refer to this
+      automatically-collected information as "Device Information." </p>
+    <br/>
+    <p className='text-left mx-5'> We collect Device Information using the following technologies: </p><br/>
+    <p className='text-left mx-5'>
+      - "Cookies" are data files that are placed on your device or computer and often include an anonymous unique
+      identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org. </p><br/>
+    <p className='text-left mx-5'>
+        - "Log files" track actions occurring on the Site, and collect data including your IP address, browser type, Internet
+      service provider, referring/exit pages, and date/time stamps. </p> <br/>
+    <p className='text-left mx-5'>
+    - "Web beacons," "tags," and "pixels" are electronic files used to record information about how you browse the Site. </p> <br/>
+    <p className='text-left mx-5'>
+      Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain
+      information from you, including your name, billing address, shipping address, payment information, email address,
+      and phone number. We refer to this information as "Order Information."  </p><br/>
+      <p className='text-left mx-5'>When we talk about "Personal Information" in this Privacy Policy, we are talking both about Device Information and
+        Order Information.
+      </p><br/>
+      <p className='text-left mx-5'> HOW DO WE USE YOUR PERSONAL INFORMATION? </p><br/>
 
+      <p className='text-left mx-5'>We use the Order Information that we collect generally to fulfill any orders placed through the Site (including
+        processing your payment information, arranging for shipping, and providing you with invoices and/or order
+        confirmations). Additionally, we use this Order Information to:
+        Communicate with you; </p><br/>
+        <p className='text-left mx-5'>Screen our orders for potential risk or fraud; and  </p><br/>
+        <p className='text-left mx-5'>When in line with the preferences you have shared with us, provide you with information or advertising relating to
+        our products or services. </p> <br/>
+        <p className='text-left mx-5'> We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP
+          address), and more generally to improve and optimize our Site (for example, by generating analytics about how our
+            customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).
+            We also use the Device Information to target you for our future advertising / remarketing campaigns. </p> <br/>
 
+            <p className='text-left mx-5'>SHARING YOUR PERSONAL INFORMATION </p> <br/>
+            <p className='text-left mx-5'>We share your Personal Information with third parties to help us use your Personal Information, as described above.
+            We use Google Analytics to help us understand how our customers use the Site-you can read more about how
+            Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-
+            out of Google Analytics here: https://tools.google.com/dlpage/gaoptout. </p> <br/>
+            <p className='text-left mx-5'>Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a
+            subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights. </p> <br/>
+            <p className='text-left mx-5'> As described above, we use your Personal Information to provide you with targeted advertisements or marketing
+            communications we believe may be of interest to you. For more information about how targeted advertising works,
+            you can visit the Network Advertising Initiative's ("NAI") educational page at
+            http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work. </p> <br/>
 
-- "Cookies" are data files that are placed on your device or computer and often include an anonymous unique
-identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.
-- "Log files" track actions occurring on the Site, and collect data including your IP address, browser type, Internet
-service provider, referring/exit pages, and date/time stamps.
-- "Web beacons," "tags," and "pixels" are electronic files used to record information about how you browse the Site.
-Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain
-information from you, including your name, billing address, shipping address, payment information, email address,
-and phone number. We refer to this information as "Order Information."
-When we talk about "Personal Information" in this Privacy Policy, we are talking both about Device Information and
-Order Information.
-HOW DO WE USE YOUR PERSONAL INFORMATION?
+            <p className='text-left mx-5'>You can opt out of targeted advertising by visiting these links: </p> <br/>
+            <p className='text-left mx-5'> FACEBOOK - https://www.facebook.com/settings/?tab=ads  </p><br/>
+            <p className='text-left mx-5'> GOOGLE - https://www.google.com/settings/ads/anonymous </p> <br/>
+            <p className='text-left mx-5'> BING - https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads  </p><br/>
+            <p className='text-left mx-5'>Additionally, you can opt out of some of these services by visiting the Digital Advertising Alliance's opt-out portal at: <br/>
+            http://optout.aboutads.info/.  </p><br/>
+            <p className='text-left mx-5'> DO NOT TRACK  </p><br/>
+            <p className='text-left mx-5'> Please note that we do not alter our Site's data collection and use practices when we see a Do Not Track signal from
+            your browser. </p> <br/>
 
-We use the Order Information that we collect generally to fulfill any orders placed through the Site (including
-processing your payment information, arranging for shipping, and providing you with invoices and/or order
-confirmations). Additionally, we use this Order Information to:
-Communicate with you;
-Screen our orders for potential risk or fraud; and
-When in line with the preferences you have shared with us, provide you with information or advertising relating to
-our products or services.
-We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP
-address), and more generally to improve and optimize our Site (for example, by generating analytics about how our
-customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).
-We also use the Device Information to target you for our future advertising / remarketing campaigns.
-
-SHARING YOUR PERSONAL INFORMATION
-We share your Personal Information with third parties to help us use your Personal Information, as described above.
-We use Google Analytics to help us understand how our customers use the Site-you can read more about how
-Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-
-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.
-Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a
-subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.
-As described above, we use your Personal Information to provide you with targeted advertisements or marketing
-communications we believe may be of interest to you. For more information about how targeted advertising works,
-you can visit the Network Advertising Initiative's ("NAI") educational page at
-http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
-
-You can opt out of targeted advertising by visiting these links:
-FACEBOOK - https://www.facebook.com/settings/?tab=ads
-GOOGLE - https://www.google.com/settings/ads/anonymous
-BING - https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads
-Additionally, you can opt out of some of these services by visiting the Digital Advertising Alliance's opt-out portal at:
-http://optout.aboutads.info/.
-DO NOT TRACK
-Please note that we do not alter our Site's data collection and use practices when we see a Do Not Track signal from
-your browser.
-
-YOUR RIGHTS
-If you are a European resident, you have the right to access personal information we hold about you and to ask that
-your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us
-through the contact information below.
-Additionally, if you are a European resident we note that we are processing your information in order to fulfill
+            <p className='text-left mx-5'>YOUR RIGHTS </p> <br/>
+            <p className='text-left mx-5'>If you are a European resident, you have the right to access personal information we hold about you and to ask that
+            your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us
+            through the contact information below. </p><br/>
+            <p className='text-left mx-5'>Additionally, if you are a European resident we note that we are processing your information in order to fulfill
 contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our
 legitimate business interests listed above. Additionally, please note that your information will be transferred outside of
-Europe, including to Canada and the United States.
-DATA RETENTION
-When you place an order through the Site, we will maintain your Order Information for our records unless and until
-you ask us to delete this information.
-MINORS
-The Site is not intended for individuals under the age of 18.
-CHANGES
-We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for
-other operational, legal or regulatory reasons.
-CONTACT US
-For more information about our privacy practices, if you have questions, or if you would like to make a complaint,
-please contact us by e-mail at operations@theveshticompany.com or by mail using the details provided below:
-59/11, 1st floor, South Avani Moola Street, Madurai - 625001
+            Europe, including to Canada and the United States.  </p><br/>
+            <p className='text-left mx-5'> DATA RETENTION  </p><br/>
+            <p className='text-left mx-5'>When you place an order through the Site, we will maintain your Order Information for our records unless and until
+            you ask us to delete this information.  </p><br/>
+            <p className='text-left mx-5'>MINORS </p> <br/>
+            <p className='text-left mx-5'>The Site is not intended for individuals under the age of 18. </p> <br/>
+            <p className='text-left mx-5'> CHANGES  </p><br/>
+            <p className='text-left mx-5'>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for
+            other operational, legal or regulatory reasons.  </p><br/>
+            <p className='text-left mx-5'>CONTACT US </p> <br/>
+            <p className='text-left mx-5'>For more information about our privacy practices, if you have questions, or if you would like to make a complaint,
+            please contact us by e-mail at operations@theveshticompany.com or by mail using the details provided below:
+            59/11, 1st floor, South Avani Moola Street, Madurai - 625001 </p><br/>
     </div>
   </>)
 }
