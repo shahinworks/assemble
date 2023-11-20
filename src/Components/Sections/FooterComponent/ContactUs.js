8d2ff654cb39dddd70d547dc5111497947b04353
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-20 10:43:44 +0530

    c

--- src/Components/Sections/FooterComponent/ContactUs.js
+++ src/Components/Sections/FooterComponent/ContactUs.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function ContactUs() {
-  return (
-    <div>ContactUs</div>
-  )
+  return (<>
+    <div className='text-left px-3 m-auto mt-5' style={{ paddingTop: "10%", border: "none", width: "70%", borderRadius: "5px"}}>
+      <h1 className='text-center fw-bold' > Contact Us</h1>
+    </div>
+  </>);
 }
 
-export default ContactUs
+export default ContactUs;
