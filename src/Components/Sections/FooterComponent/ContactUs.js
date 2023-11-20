2be1f3ef71cf3dcdc055e2622cb925ecb40964f4
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-20 15:09:02 +0530

    c

--- src/Components/Sections/FooterComponent/ContactUs.js
+++ src/Components/Sections/FooterComponent/ContactUs.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Card, Form } from 'react-bootstrap';
 
 function ContactUs() {
   return (<>
     <div className='text-left px-3 m-auto mt-5' style={{ paddingTop: "10%", border: "none", width: "70%", borderRadius: "5px"}}>
       <h1 className='text-center fw-bold' > Contact Us</h1>
+      <Card>
+        <Card.Body>
+          <Form>
+            <Form.Label>Email</Form.Label>
+            <Form.Control />
+          </Form>
+        </Card.Body>
+      </Card>
     </div>
   </>);
 }
