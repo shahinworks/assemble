a47db39cf8bfdb1f2b65bea61fe47632c73e0a2d
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-22 14:53:20 +0530

    c

--- src/Components/Home/CheckOutResponse/Failure.js
+++ src/Components/Home/CheckOutResponse/Failure.js
@@ -13,7 +13,7 @@ function Failure() {
   return (
     <>
       <div className="text-center">
-        <img style={{ height: '100px', width: '100px', borderRadius: '50px' }} alt="tick" src={img} />
+        <img style={{ height: '125px', width: '125px', borderRadius: '50px', marginTop: "10%" }} alt="tick" src={img} />
         <h1 className="my-2 mx-2">Payment Failed</h1>
       </div>
     </>
--- src/Components/Home/CheckOutResponse/Success.js
+++ src/Components/Home/CheckOutResponse/Success.js
@@ -15,7 +15,7 @@ function Success() {
   return (
     <>
       <div className="text-center">
-        <img style={{ height: '125px', width: '100px', borderRadius: '50px', marginTop: "10%" }} alt="tick" src={img} />
+        <img style={{ height: '175px', width: '140px', borderRadius: '50px', marginTop: "10%" }} alt="tick" src={img} />
         <h1 className="my-2 mx-2">Payment Successful</h1>
       </div>
     </>
