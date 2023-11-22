c71ab82aa46bef849a1582383cf9e9910d8890e7
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-22 15:41:15 +0530

    C

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -106,11 +106,16 @@ function Home() {
     <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
       <div className="carousel-inner">
       {homePageSlider?.getAllHomePageSlider && homePageSlider?.getAllHomePageSlider[0]?.images && <div className="carousel-item active">
-          <img className="d-block w-100" style={{height: "700px", width: "1200px"}} src={homePageSlider?.getAllHomePageSlider[0]?.images} alt="First slide" />
+          <img className="d-block w-100" 
+          // style={{height: "700px", width: "1200px"}} 
+          src={homePageSlider?.getAllHomePageSlider[0]?.images} alt="First slide" />
         </div>}
       {homePageSlider?.getAllHomePageSlider &&  homePageSlider?.getAllHomePageSlider?.map((item, index) => index > 0 &&
         <div key={item.id} className="carousel-item">
-          <img className="d-block w-100"  style={{height: "700px", width: "1200px"}} src={item.images} alt="Second slide" />
+          <img className="d-block w-100" 
+          // style={{height: "30%"}} 
+         // style={{height: "700px", width: "1200px"}} 
+          src={item.images} alt="Second slide" />
         </div>)} 
       </div>
       <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
