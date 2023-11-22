4cfed868f1839a748b86dd1d9f3b12f82422d4b4
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-22 12:21:15 +0530

    c

--- src/Components/Home/CheckOutResponse/Success.js
+++ src/Components/Home/CheckOutResponse/Success.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Cartitems from 'globalValue/CartItems/Cartitems';
+import img from './success.png';
 
 function Success() {
+  const { refetch } = Cartitems();
+  const history = useHistory();
+  useEffect(() => {
+    refetch();
+    setTimeout(() => {
+      history.push('/shop/order');
+    }, [4000]);
+  }, []);
   return (
-    <div>Success</div>
-  )
+    <>
+      <div className="text-center">
+        <img style={{ height: '100px', width: '100px', borderRadius: '50px' }} alt="tick" src={img} />
+        <h1 className="my-2 mx-2">Payment Successful</h1>
+      </div>
+    </>
+  );
 }
 
-export default Success
+export default Success;
