5d4f8518c2c94af2c2f34113e4807bf5647d3404
Shahin Shaikh <shahinwithani07@gmail.com>
2023-11-23 15:14:01 +0530

    c

--- src/Components/Demo/Demo.js
+++ src/Components/Demo/Demo.js
@@ -1,91 +1,40 @@
-import React, { useState } from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import { gql, useQuery } from '@apollo/client'; 
-import { useEffect } from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Button } from 'flowbite-react';
 
 function Demo() {
+  return (
+    <div>
+      <Navbar fluid rounded>
+      <Navbar.Brand as={Link} href="https://flowbite-react.com">
+        {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link as={Link} href="#">
+          About
+        </Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+      </Navbar>
+      {/* <Button outline gradientDuoTone="purpleToPink">
+        Purple to Pink
+      </Button> */}
+      {/* <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+Pink to orange
+</span>
+      </button>
 
-  const GET_ALL_PRODUCT = gql`
-  query GetAllProducts {
-    getAllProducts {
-      id
-      color
-      description
-      discount
-      gender
-      gst
-      priveiwName
-      productName
-      sellingPrice
-      size
-      stock {
-        quantity
-        gender
-        color
-        size
-      }
-      images {
-        imagePath
-        color
-        gender
-      }
-    }
-  }
-`;
-
-
-const [data, setData ] = useState([]);
-
-const { data: product, refetch } = useQuery(GET_ALL_PRODUCT);
-
-
-useEffect(() => {
-  setData(product?.getAllProducts);
-}, [product]);
-
-
-
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
-  return (<>
-  
-    {data && <Carousel responsive={responsive}>
-    {data && data?.map((data, index) => <div key={index} >
-          <img src={data?.images[0]?.imagePath[0]} alt="Image 2" />
-        </div> )}
-       
-    </Carousel>}
-   
-  </>);
+     <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button> */}
+    </div>
+  )
 }
 
-export default Demo;
-
-
-  //  <div  >
-  //         <img src="https://source.unsplash.com/300x300/?perth" alt="Image 2" />
-  //       </div>
-  //       <div  >
-  //         <img src="https://source.unsplash.com/300x300/?fremantle,australia" alt="Image 1" />
-  //       </div>
-  //       <div  >
-  //         <img src="https://source.unsplash.com/300x300/?perth" alt="Image 3" />
-  //       </div>
+export default Demo
